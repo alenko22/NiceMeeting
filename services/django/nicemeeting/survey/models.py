bda79c3585990f080d93b5ee9866ba1a6306965b
alenko22 <pulyatv1@gmail.com>
2026-02-26 14:54:22 +0500

    Modified models

--- services/django/nicemeeting/survey/models.py
+++ services/django/nicemeeting/survey/models.py
@@ -6,30 +6,3 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-class User(models.Model):
-    data = models.JSONField()
-
-    class Meta:
-        managed = True
-        db_table = 'survey"."user'
-
-class Questionnare(models.Model):
-    data = models.JSONField()
-
-    class Meta:
-        managed = True
-        db_table = 'survey"."questionnare'
-
-
-class Rating(models.Model):
-    id = models.AutoField(primary_key=True)
-    rating = models.SmallIntegerField()
-    id_user = models.ForeignKey('User', models.DO_NOTHING, db_column='id_user')
-    id_questionnare = models.ForeignKey('Questionnare', models.DO_NOTHING, db_column='id_questionnare')
-
-    class Meta:
-        managed = True
-        db_table = 'survey"."rating'
-
-
